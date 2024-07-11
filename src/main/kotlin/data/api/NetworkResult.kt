6d655a4e9fdc83a01198d725604c0e7708b7344e
Mazid <m3farahani1382@gmail.com>
2024-07-11 15:11:57 +0330

    Implements the NetworkResult sealed class.

--- src/main/kotlin/data/api/NetworkResult.kt
+++ src/main/kotlin/data/api/NetworkResult.kt
@@ -1,6 +1,6 @@
 package org.example.data.api
 
-sealed class NetworkResult<out T : Any> {
-    // TODO: Search about error handling patterns and implement this class.
-    // TODO: Adds Success and Failure result.
+sealed class NetworkResult<out T> {
+    data class Success<out T>(val data: T) : NetworkResult<T>()
+    data class Error<out T>(val exception: Exception) : NetworkResult<T>()
 }
